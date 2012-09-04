5d1d080faf39ff0fc70ea70b5f7b1eda05cbf0bd
Owen Trueblood <trueowen@verizon.net>
2012-09-04 16:33:02 -0400

    Removed garbage from the assembler.

--- AvrBuild.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"C:\Program Files\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "C:\Projects\Hardware\Blue_TILP\labels.tmp" -fI -W+ie -o "C:\Projects\Hardware\Blue_TILP\blue_tilp.hex" -d "C:\Projects\Hardware\Blue_TILP\blue_tilp.obj" -e "C:\Projects\Hardware\Blue_TILP\blue_tilp.eep" -m "C:\Projects\Hardware\Blue_TILP\blue_tilp.map" "C:\Projects\Hardware\Blue_TILP\blue_tilp.asm"
